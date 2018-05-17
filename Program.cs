960743f228fdca2ac7c0a4ac1359b9b973cf9444
Drake Lambert <drake.askme2@gmail.com>
2018-05-17 13:23:38 -0500

    using lambdas

--- Program.cs
+++ Program.cs
@@ -2,51 +2,26 @@
 
 namespace FizzBuzz
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			int count = 0;
-			if (args.Length < 1)
-			{
-				return;
-			}
-			if (!int.TryParse(args[0], out count))
-			{
-				if (args.Length < 2)
-				{
-					return;
-				}
-				if (!int.TryParse(args[1], out count))
-				{
-					return;
-				}
-			}
-			string str;
-			bool isSet;
-			for (var i = 1; i <= count; i++)
-			{
-				str = string.Empty;
-				isSet = false;
-				if (i % 3 == 0)
-				{
-					str = "Fizz";
-					isSet = true;
-				}
-				if (i % 5 == 0)
-				{
-					str += "Buzz";
-					isSet = true;
-				}
-				if (!isSet)
-				{
-					Console.WriteLine(i);
-				}
-				else
-				{   
-					Console.WriteLine(str);
-				}
-			}
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 15;
+
+            Func<int, string> isFizz = i => i % 3 == 0 ? "Fizz" : "";
+
+            Func<int, string> isBuzz = i => i % 5 == 0 ? "Buzz" : "";
+
+            var fizzBuzz = "";
+            for (var i = 1; i <= count; i++)
+            {
+                fizzBuzz = isFizz(i) + isBuzz(i);
+                if (fizzBuzz.Length == 0)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine(fizzBuzz);
+            }
+        }
+    }
 }
